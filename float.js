086e97f92c60e6474967df80ced8f83c4dff96bc
esohjay <59736116+esohjay@users.noreply.github.com>
2020-03-22 17:29:41 +0100

    Add files via upload

---
+++ float.js
@@ -0,0 +1,10 @@
+const navslide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".navlinks");
+
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("toggle")
+  });}
+  navslide();
